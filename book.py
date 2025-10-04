fe2413522af2c084945fc13e25a25873fd5369ee
RicardoAly55 <ricardoaly9@gmail.com>
2025-10-04 00:49:50 +0000

    test RF4

--- book.py
+++ book.py
@@ -9,11 +9,16 @@ class Book:
     def borrow(self):
         if self.borrowed:
             raise ValueError("The book is already borrowed")
+        else:
+            print("the borrow is not borrowed")
         self.borrowed = True
 
     def return_book(self):
         if not self.borrowed:
             raise ValueError("The book is not currently borrowed")
         self.borrowed = False
+if __name__ == "__main__":
+    libro=Book("El racismo de Mischa","Aly",2023,"isbn0001")
+    libro.borrow()
 
 
