accd5f26b8e74ffd4da0a2abf10a571bf0d79d37
RicardoAly55 <ricardoaly9@gmail.com>
2025-10-04 01:22:28 +0000

    test doble borrow

--- Test_int.py
+++ Test_int.py
@@ -7,3 +7,4 @@ if __name__ == "__main__":
     libro = Book("To Kill a Mockingbird", "Harper Lee", 1960, "ISBN002")
     liba.add_book(Book("To Kill a Mockingbird", "Harper Lee", 1960, "ISBN002"))
     libro.borrow()
+    libro.borrow()
